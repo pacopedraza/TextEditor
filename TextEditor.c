c0898d21be2a55625f46225b43baddfd7073386e
Francisco Pedraza <pacopedraza87@gmail.com>
2020-08-06 12:53:23 -0700

    Refactors keyboard input
    Removed code from main function and call a function in main
    instead.
    editorReadKey() usage is to wait for one keypress, and return it.
    We could expand this to handle escape sequences.
    editorProcessKeyPress() waits for a keypress, and then handles it.
    It can map various Ctrl key combinations and other special keys
    to different editor functions.
    
    Signed-off-by: Francisco Pedraza <pacopedraza87@gmail.com>

--- TextEditor.c
+++ TextEditor.c
@@ -43,19 +43,34 @@ void enablingRawMode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) kill("tcsetattr");
 }
 
+char editorReadKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) kill("read");
+    }
+    return c;
+}
+
+/*** input ***/
+
+void editorProcessKeyPressing() {
+    char c = editorReadKey();
+
+    switch (c)
+    {
+    case CTRL_KEY('q'):
+        exit(0);
+        break;
+    }
+}
+
 /*** init ***/
 int main() {
     enablingRawMode();
 
     while (1) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) kill("read");
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == CTRL_KEY('q')) break;
+        editorProcessKeyPressing();
     }
 
     return 0;
