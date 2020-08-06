51d2d26fa1bd7590d031707fb2bb1f0ae84e0939
Francisco Pedraza <pacopedraza87@gmail.com>
2020-08-05 19:22:33 -0700

    Adds Setup, Makefile and raw mode
    Added rawmode for reading until the user applies CTRL-D or
    press 'q'.
    
    Signed-off-by: Francisco Pedraza <pacopedraza87@gmail.com>

---
+++ Makefile
@@ -0,0 +1,2 @@
+TestEditor: TestEditor.c
+	$(CC) TestEditor.c -o TestEditor -Wall -Wextra -pedantic -std=c99
---
+++ TestEditor.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
+
+int main() {
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    return 0;
+}
